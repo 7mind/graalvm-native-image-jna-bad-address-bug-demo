f72ca30e0dffc9f85fe8e93cfe35fd52b64a30f2
Pavel Shirshov <pshirshov@eml.cc>
2022-02-02 17:31:11 +0000

    working demo (netty transport)

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -2,41 +2,21 @@ package demo;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
-import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.core.DockerClientImpl;
-//import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
-import com.github.dockerjava.transport.DockerHttpClient;
-import com.github.dockerjava.zerodep.ZerodepDockerHttpClient;
+import com.github.dockerjava.netty.NettyDockerCmdExecFactory;
 
-import java.time.Duration;
+import java.io.IOException;
 
 public class Demo {
-    public static void main(String[] args) {
-        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+    public static void main(String[] args) throws IOException {
+        DefaultDockerClientConfig configBuilder = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+        NettyDockerCmdExecFactory factory = new NettyDockerCmdExecFactory();
 
-//        DockerClientFactory
-        DockerClient client = DockerClientImpl.getInstance(
-                config,
-new ZerodepDockerHttpClient.Builder().dockerHost(config.getDockerHost()).sslConfig(config.getSSLConfig()).build()
-                );
+        DockerClient client = DockerClientImpl.getInstance(configBuilder).withDockerCmdExecFactory(factory);
+//                new OkDockerHttpClient.Builder().dockerHost(configBuilder.getDockerHost()).sslConfig(configBuilder.getSSLConfig()).build()
+
 
         System.out.println(client.listImagesCmd().exec());
-//        DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
-//                .dockerHost(config.getDockerHost())
-//                .sslConfig(config.getSSLConfig())
-//                .maxConnections(100)
-//                .connectionTimeout(Duration.ofSeconds(30))
-//                .responseTimeout(Duration.ofSeconds(45))
-//                .build();
-//
-//        DockerHttpClient.Request request = DockerHttpClient.Request.builder()
-//                .method(DockerHttpClient.Request.Method.GET)
-//                .path("/_ping")
-//                .build();
-//
-//        try (DockerHttpClient.Response response = httpClient.execute(request)) {
-//            assertThat(response.getStatusCode(), equalTo(200));
-//            assertThat(IOUtils.toString(response.getBody()), equalTo("OK"));
-//        }
+        factory.close();
     }
 }
